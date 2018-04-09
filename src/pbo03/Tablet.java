4ddaa86c9304222c39c93c26d572b87d35e5a80a
inasrenan01 <37865387+inasrenan01@users.noreply.github.com>
2018-04-09 23:40:38 +0800

    membuat atribut tablet

--- src/pbo03/Tablet.java
+++ src/pbo03/Tablet.java
@@ -1,5 +1,11 @@
 package pbo03;
 
-public class Tablet {
+public class Tablet  extends smartphone{
+    private String jenismartphone;
+    private String ukuranlayar;
+    
+    void throttle(){
+        System.out.println("tablet loo...");
+    }
     
 }
