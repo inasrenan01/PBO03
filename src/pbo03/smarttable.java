bb4895616d0613ab1da5d09f29b2dba0d591374c
inasrenan01 <37865387+inasrenan01@users.noreply.github.com>
2018-04-10 00:14:56 +0800

    get end set

--- src/pbo03/smarttable.java
+++ src/pbo03/smarttable.java
@@ -1,5 +1,27 @@
 package pbo03;
 
-public class smarttable {
+public class smarttable  extends smartphone{
+    private String jenismartphone;
+    private String ukuranlayar;
     
+    void throttle(){
+        System.out.println("tablet loo...");
+        
+    }
+
+    public String getJenismartphone() {
+        return jenismartphone;
+    }
+
+    public void setJenismartphone(String jenismartphone) {
+        this.jenismartphone = jenismartphone;
+    }
+
+    public String getUkuranlayar() {
+        return ukuranlayar;
+    }
+
+    public void setUkuranlayar(String ukuranlayar) {
+        this.ukuranlayar = ukuranlayar;
+    }
 }
