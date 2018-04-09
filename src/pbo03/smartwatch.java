8bf7fee551ac469dbb83dd56f2ab81d8e2d698bc
inasrenan01 <37865387+inasrenan01@users.noreply.github.com>
2018-04-10 00:26:35 +0800

    membuat atribut get ean set

--- src/pbo03/smartwatch.java
+++ src/pbo03/smartwatch.java
@@ -9,4 +9,28 @@ public class smartwatch extends smartphone{
         System.out.println("ini hp jamtangan  loo...");
         
     }
+
+    public String getJenismartphone() {
+        return jenismartphone;
+    }
+
+    public void setJenismartphone(String jenismartphone) {
+        this.jenismartphone = jenismartphone;
+    }
+
+    public String getUkuranlayar() {
+        return ukuranlayar;
+    }
+
+    public void setUkuranlayar(String ukuranlayar) {
+        this.ukuranlayar = ukuranlayar;
+    }
+
+    public String getTipejam() {
+        return tipejam;
+    }
+
+    public void setTipejam(String tipejam) {
+        this.tipejam = tipejam;
+    }
 }
