875207cdd525568bde79ac675ced18751d30d480
inasrenan01 <37865387+inasrenan01@users.noreply.github.com>
2018-04-09 22:20:10 +0800

    memebuat atribut

--- src/pbo03/smartphone.java
+++ src/pbo03/smartphone.java
@@ -1,14 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pbo03;
 
-/**
- *
- * @author m.nasrullah
- */
 public class smartphone {
+    private String merk;
+    private String warna;
+    private String memory;
+    private double harga;
+    
+    
+    
+ 
     
 }
