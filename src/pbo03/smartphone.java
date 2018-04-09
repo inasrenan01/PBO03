66519a1a6603a1887a2e00a232131084d4ae818b
inasrenan01 <37865387+inasrenan01@users.noreply.github.com>
2018-04-09 23:12:32 +0800

    membuat getter dan setter

--- src/pbo03/smartphone.java
+++ src/pbo03/smartphone.java
@@ -13,4 +13,21 @@ public class smartphone {
                 "Ukuran \t:"+memory+"\n"+
                 "Harga \t:"+harga); 
     }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public void setMemory(String memory) {
+        this.memory = memory;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    
 }
