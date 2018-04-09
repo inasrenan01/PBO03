9d26fe4b199560cf0f4445cb018b1318430e247c
inasrenan01 <37865387+inasrenan01@users.noreply.github.com>
2018-04-09 22:58:48 +0800

    intennce, memebuat method dan memanggil atribut

--- src/pbo03/smartphoneAksi.java
+++ src/pbo03/smartphoneAksi.java
@@ -1,4 +1,13 @@
 package pbo03;
 public class smartphoneAksi {
+    public static void main(String[] args) {
+    smartphone a = new smartphone();
     
+        a.setMerk("oppo");
+        a.setWarna("hitam");
+        a.setMemory("16GB");
+        a.setHarga(1500000);
+        
+        a.cetakInfo();
+    }
 }
