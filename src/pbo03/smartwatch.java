6cebf982943921d17698e39dd7ae2d588093d5bf
inasrenan01 <37865387+inasrenan01@users.noreply.github.com>
2018-04-10 00:21:10 +0800

    memebuat class dan atribut smartwatch exstend smartphone

--- src/pbo03/smartwatch.java
+++ src/pbo03/smartwatch.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pbo03;
 
-/**
- *
- * @author ikhsan
- */
-public class smartwatch {
+public class smartwatch extends smartphone{
+    private String jenismartphone;
+    private String ukuranlayar;
+    private String tipejam;
     
+      void throttle(){
+        System.out.println("ini hp jamtangan  loo...");
+        
+    }
 }
